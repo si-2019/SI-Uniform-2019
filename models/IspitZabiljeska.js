2ab91de36389ddddc5dd0382eb314cc4ffca2389
emehic3 <mehicemina97@gmail.com>
2019-05-27 15:36:51 +0200

    US58

--- models/IspitZabiljeska.js
+++ models/IspitZabiljeska.js
@@ -2,7 +2,11 @@
 
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('IspitZabiljeska', {
-  
+    idIspitZabiljeska: {
+      type: DataTypes.INTEGER(10),
+      allowNull: false,
+      primaryKey: true
+    },
     idIspit: {
       type: DataTypes.INTEGER(10),
       allowNull: true,
