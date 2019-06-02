311c3b42d6d213365f4b0035353026091769b989
emehic3 <mehicemina97@gmail.com>
2019-06-02 23:04:17 +0200

    sprint 8

--- models/PonudjeniOdgovor.js
+++ models/PonudjeniOdgovor.js
@@ -3,6 +3,12 @@
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('PonudjeniOdgovor', {
     
+idPonudjeniOdgovor: {
+  type: DataTypes.INTEGER(10),
+  allowNull: false,
+  primaryKey: true
+},
+
     idPitanja: {
       type: DataTypes.INTEGER(10),
       allowNull: false,
