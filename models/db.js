bd40295d18813457ab0e70b628a969e274015bbe
gobathegreat <agobeljic1@etf.unsa.ba>
2019-06-15 18:53:50 +0200

    heroku

--- models/db.js
+++ models/db.js
@@ -1,12 +1,13 @@
 
-const Sequelize = require("sequelize");
+/*const Sequelize = require("sequelize");
 const sequelize = new Sequelize("TYQcLL35gV","TYQcLL35gV","BLysSj9ZrP",{host:'37.59.55.185',dialect:"mysql",logging:false,  port: 3306,define: {
     timestamps: false
 }
-});
+});*/
 const db = {}
+/*
 db.Sequelize = Sequelize;  
-db.sequelize = sequelize;
+db.sequelize = sequelize;*/
 
 //import modela
 /*
