9267c761b6ba0738bf2723aebf4723ebd7182164
gobathegreat <agobeljic1@etf.unsa.ba>
2019-06-15 18:08:59 +0200

    heroku

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
-/*var pug = require('pug');*/
+/*var pug = require('pug');                         -------------*/
 var bodyParser = require('body-parser');
-var upload = require('express-fileupload');
-/*var cors = require('cors');
-var axios=require('axios');*/
+/*var upload = require('express-fileupload');       -------------*/
+var cors = require('cors');
+/*var axios=require('axios');*/
 var port = process.env.PORT || 31920;
 /*
 db.sequelize.sync().then(function(){
