96a6d5f0c12ea8b98645f0439730617d09a041da
gobathegreat <agobeljic1@etf.unsa.ba>
2019-06-15 18:58:57 +0200

    heroku

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("TYQcLL35gV","TYQcLL35gV","BLysSj9ZrP",{host:'37.59.55.185',dialect:"mysql",logging:false,  port: 3306,define: {
+const sequelize = new Sequelize("TYQcLL35gV","TYQcLL35gV","BLysSj9ZrP",{host:'37.59.55.185',dialect:"mysql",timezone: '+01:00',define: {
     timestamps: false
 }
 });
