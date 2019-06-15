d1fe0f735540864e89911694ae9063b4fa072859
gobathegreat <agobeljic1@etf.unsa.ba>
2019-06-15 17:31:36 +0200

    heroku

--- index.js
+++ index.js
@@ -2,24 +2,3 @@ const http = require('http');
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-/*const port = process.env.PORT || 3000;
-
-/*
-var app = express();
-app.listen(port, function(){ console.log('server successfully started on port '+port); });
-
-*/
-
-
-
