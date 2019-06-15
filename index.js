250970e8c6c7c2cacdf2f9bed7ef1b863f7964ef
gobathegreat <agobeljic1@etf.unsa.ba>
2019-06-15 17:26:03 +0200

    heroku

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 
+/*
 var app = express();
 app.listen(port, function(){ console.log('server successfully started on port '+port); });
 
-
+*/
 
 
 
