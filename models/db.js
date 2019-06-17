290a5a7cba090f265039b67d89773eda74e22cac
agobeljic1 <37115535+agobeljic1@users.noreply.github.com>
2019-06-17 16:55:33 +0200

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("TYQcLL35gV","TYQcLL35gV","BLysSj9ZrP"",{host:'37.59.55.185',dialect:"mysql",logging:false,  port: 3306,define: {
+const sequelize = new Sequelize("TYQcLL35gV","TYQcLL35gV","BLysSj9ZrP",{host:'37.59.55.185',dialect:"mysql",logging:false,  port: 3306,define: {
     timestamps: false
 }
 });
